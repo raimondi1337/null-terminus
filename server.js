00c4826339036f0850001e3ae7c348c5ccb86344
Dustin Raimondi <raimondi1337@gmail.com>
2016-08-25 16:20:28 -0700

    serve whole public directory, move JS into its own file

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var clients = {};
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
